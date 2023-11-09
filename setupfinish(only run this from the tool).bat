f4a9827522387ae279832a3b88e8942035f52409
472XYZ <149680310+472XYZ@users.noreply.github.com>
2023-11-09 19:29:22 +0100

    Tool DONE

---
+++ setupfinish(only run this from the tool).bat
@@ -0,0 +1,9 @@
+@echo off
+echo FINISHING SETUP
+python setupfinish.py
+echo ENJOY USING THE 472 TOOL
+echo.
+echo Please note that the average time for downloading and installing all the dependencies is approximately 2 minutes. 
+echo.
+timeout /t 180 >nul
+echo Thank you for using the tool!
---
+++ start_tool.bat
@@ -0,0 +1,3 @@
+python atio.py 
+python cdd.py 
+python 472.py 
